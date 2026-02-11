bc77fd9b2f774eb62fe91b6853c242f4a04253b4
Landaverde-04 <landaverde.kevin04@gmail.com>
2026-02-10 18:36:49 -0600

    Inicializacion de funciones basicas y suma

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,27 @@
 #Inicializacion
+
+def peticion_usuario():
+    print("Bienvenido a la calculadora")
+    print("Ingrese el primer numero")
+    num1 = float(input())
+    print("Ingrese el segundo numero")
+    num2 = float(input())
+    return num1, num2
+
+def opciones():
+    print("Seleccione la operacion a realizar")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicacion")    
+    opcion = int(input())
+    return opcion
+
+def suma(num1, num2):
+    return num1 + num2
+
+
+num1, num2 = peticion_usuario()
+opcion = opciones()
+if opcion == 1:
+    resultado = suma(num1, num2)
+    print("El resultado de la suma es: ", resultado)
